46cd248e3cd118e91c5b4f92d84e7468259588f2
Addai Christopher <addaichristopher1@gmail.com>
2023-10-11 01:08:01 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
 #include "shell.h"
 
-int main(char **argv)
+int main(int ac, char **argv)
 {
 	char *cmd = "$ ";
-	char *inputptr;
+	char *inputptr = NULL, *inputptr2nd = NULL;
 	size_t buffsize = 32;
 	ssize_t input_len;
 	char *input_token;
-	(void) argv;
+	int j, token_len = 0;
+	const char *delimiter = " ";
+
+	(void) ac;
+
 
 	while (1)
 	{
@@ -15,23 +19,56 @@ int main(char **argv)
 		input_len = getline(&inputptr, &buffsize, stdin);
 		
 		if (input_len == -1)
+		{
+			printf("existing...\n");
 			exit(1);
+		}
 		
-		printf("You typed: %s\n", inputptr);
-		input_token = strtok(*input," ");
+		inputptr2nd = malloc(sizeof(char) * input_len);
+
+		if(inputptr2nd == NULL)
+		{
+			perror("memory allocation error!");
+			return (-1);
+		}
 		
-		argv = malloc(sizeof(char *) *input_len);
-		for (int j = 0; input_token != NULL; j++)
+		strcpy(inputptr2nd, inputptr);
+
+		input_token = strtok(inputptr2nd, delimiter);
+
+		while (input_token != NULL)
+		{
+			token_len ++;
+			input_token = strtok(NULL, delimiter);
+		}
+		token_len++;
+		
+		argv = malloc(sizeof(char *) * token_len);
+
+		input_token = strtok(inputptr2nd, delimiter);
+
+		for ( j = 0; input_token != NULL; j++)
 		{
 			argv[j] = malloc(sizeof(char) * strlen(input_token));
 			strcpy(argv[j], input_token);
-			input_token = strtok(NULL, " ");
+			input_token = strtok(NULL, delimiter);
 		}
 		argv[j] = NULL;
+
+
+		for (int i = 0; i < token_len -1; i++)
+		{
+			printf("%s, ", argv[i]);
+		}
+		printf("\n");
+		
 	}
 
+		free(inputptr);
+		free(argv);
+		free(inputptr2nd);
 
-	free(inputptr);
+	
 	return (0);
 	
 }
