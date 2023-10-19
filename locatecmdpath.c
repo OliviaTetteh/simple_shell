c86f7503e598fa2035db48694ddecd159b28d13f
Addai Christopher <addaichristopher1@gmail.com>
2023-10-19 23:58:06 +0000

    Update locatecmdpath.c

--- locatecmdpath.c
+++ locatecmdpath.c
@@ -11,31 +11,39 @@ char *get_cmd_path(const char *cmd)
 	char *paths_dup = strdup(paths);
 	char *p = strtok(paths_dup, ":");
 	char *file_path = NULL;
+
 	if (paths == NULL || cmd == NULL || *cmd == '\0')
 		return (NULL);
+
 	while (p)
 	{
 		size_t len = strlen(p) + strlen(cmd) + 2;
 		file_path = (char *)malloc(len);
+
 		if (file_path == NULL)
 		{
 			perror("Memory allocation error");
 			free(paths_dup);
 			return (NULL);
 		}
+
 		strcpy(file_path, p);
 		strcat(file_path, "/");
 		strcat(file_path, cmd);
+
 		if (stat(file_path, &buffer) == 0)
 		{
 			free(paths_dup);
 			return (file_path);
 		}
+
 		free(file_path);
 		file_path = NULL;
 		p = strtok(NULL, ":");
 	}
+
 	free(paths_dup);
+
 	if (stat(cmd, &buffer) == 0)
 	{
 		return (strdup(cmd));
