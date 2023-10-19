5285f8adeb02594c55b5eeb9717e4234335f52dd
OliviaTetteh <olivet36@yahoo.com>
2023-10-19 13:57:59 +0000

    locatecmdpath2.c

--- locatecmdpath2.c
+++ locatecmdpath2.c
@@ -8,33 +8,32 @@
 
 char *get_cmd_path(char *cmd)
 {
-        struct stat buffer;
-        const char *paths;
-        size_t cmd_len;
-        char *paths_dup;
-        char *p;
-        size_t dir_len;
-        char *file_path;
+	struct stat buffer;
+	const char *paths;
+	size_t cmd_len;
+	char *paths_dup;
+	char *p;
+	size_t dir_len;
+	char *file_path;
 
-                strcpy(file_path, p);
-                strcat(file_path, "/");
-                strcat(file_path, cmd);
-                if (stat(file_path, &buffer) == 0)
-                {
-                        free(paths_dup);
-                        return (file_path);
-                }
-                else
-                {
-                        free(file_path);
-                        p = strtok(NULL, ":");
-                }
-        }
+		strcpy(file_path, p);
+		strcat(file_path, "/");
+		strcat(file_path, cmd);
+		if (stat(file_path, &buffer) == 0)
+		{
+			free(paths_dup);
+			return (file_path);
+		}
+		else
+		{
+			free(file_path);
+			p = strtok(NULL, ":");
+		}
+}
 
-        free(paths_dup);
+	free(paths_dup);
 
-
-        if (stat(cmd, &buffer) == 0)
-                return (strdup(cmd));
-        return (NULL);
+	if (stat(cmd, &buffer) == 0)
+		return (strdup(cmd));
+	return (NULL);
 }
