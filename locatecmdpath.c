40a1e654126f3df5ca154a63e122ea5e254c34e7
Addai Christopher <addaichristopher1@gmail.com>
2023-10-19 22:51:20 +0000

    Update locatecmdpath.c

--- locatecmdpath.c
+++ locatecmdpath.c
@@ -9,69 +9,28 @@
 char *get_cmd_path(char *cmd)
 {
 	struct stat buffer;
-	const char *paths;
-	size_t cmd_len;
-	char *paths_dup;
-	char *p;
-	size_t dir_len;
-	char *file_path;
-
-	paths = getenv("PATH");
-
+	const char *paths = getenv("PATH");
 	if (paths == NULL || cmd == NULL || *cmd == '\0')
-		return (NULL);
+		return NULL;
 
-	cmd_len = strlen(cmd);
-	paths_dup = strdup(paths);
-	p = strtok(paths_dup, ":");
-
-	if (paths_dup == NULL)
+	char *p = strtok(strdup(paths), ":"), *file_path;
+	while (p)
 	{
-		perror("Memory allocation error");
-		return (NULL);
-	}
-
-	while (p != NULL)
-	{
-		dir_len = strlen(p);
-		file_path = (char *)malloc(cmd_len + 2 + dir_len);
-
+		file_path = (char *)malloc(strlen(p) + strlen(cmd) + 2);
 		if (file_path == NULL)
-		{
-			perror("Memory allocation error");
-			free(paths_dup);
-			return (NULL);
-		}
+			return NULL;
 
-		strcpy(file_path, p);
-		strcat(file_path, "/");
-		strcat(file_path, cmd);
-
+		sprintf(file_path, "%s/%s", p, cmd);
 		if (stat(file_path, &buffer) == 0)
 		{
-			free(paths_dup);
-			return (NULL);
+			free(p);
+			return file_path;
 		}
-
-		strcpy(file_path, p);
-		strcat(file_path, "/");
-		strcat(file_path, cmd);
-		if (stat(file_path, &buffer) == 0)
-		{
-			free(paths_dup);
-			return (file_path);
-		}
-		else
-		{
-			free(file_path);
-			p = strtok(NULL, ":");
-		}
+		free(file_path);
+		p = strtok(NULL, ":");
 	}
-
-	free(paths_dup);
-
 
 	if (stat(cmd, &buffer) == 0)
-		return (strdup(cmd));
-	return (NULL);
+		return strdup(cmd);
+	return NULL;
 }
