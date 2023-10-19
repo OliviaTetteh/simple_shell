de8f60d7436654c43d750f6e3d456a8b49d75ea8
Addai Christopher <addaichristopher1@gmail.com>
2023-10-19 23:40:51 +0000

    fix memory allocation and deallocation issues

--- locatecmdpath.c
+++ locatecmdpath.c
@@ -4,37 +4,41 @@
  * @cmd: command
  * Return: path to the cmd does not exists
  */
-char *get_cmd_path(char *cmd)
+char *get_cmd_path(const char *cmd)
 {
 	struct stat buffer;
 	const char *paths = getenv("PATH");
 	char *paths_dup = strdup(paths);
 	char *p = strtok(paths_dup, ":");
-	char *file_path;
-
+	char *file_path = NULL;
 	if (paths == NULL || cmd == NULL || *cmd == '\0')
 		return (NULL);
-
 	while (p)
 	{
-		file_path = (char *)malloc(strlen(p) + strlen(cmd) + 2);
+		size_t len = strlen(p) + strlen(cmd) + 2;
+		file_path = (char *)malloc(len);
 		if (file_path == NULL)
+		{
+			perror("Memory allocation error");
+			free(paths_dup);
 			return (NULL);
-
+		}
 		strcpy(file_path, p);
 		strcat(file_path, "/");
 		strcat(file_path, cmd);
-
 		if (stat(file_path, &buffer) == 0)
 		{
-			free(p);
+			free(paths_dup);
 			return (file_path);
 		}
-		free(file_path);
+		free(file_path);  // Free only if not needed
+		file_path = NULL; // Set to NULL to avoid double freeing
 		p = strtok(NULL, ":");
 	}
-
+	free(paths_dup);
 	if (stat(cmd, &buffer) == 0)
-		return strdup(cmd);
+	{
+		return (strdup(cmd));
+	}
 	return (NULL);
 }
