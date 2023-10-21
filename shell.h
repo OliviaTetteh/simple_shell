ee8f91f2aea307cd24555d1cd82cdde3f8ae4f1c
Addai Christopher <addaichristopher1@gmail.com>
2023-10-21 01:43:48 +0000

    comment marco
    Co-Authored-By: OliviaTetteh <138711222+OliviaTetteh@users.noreply.github.com>

--- shell.h
+++ shell.h
@@ -92,12 +92,13 @@ typedef struct passinfo
 	int readfd;
 	int histcount;
 } info_t;
-
+/*
 #define INFO_INIT                                                                       \
 	{                                                                               \
 		NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
 		    0, 0, 0                                                             \
 	}
+*/
 
 /**
  *struct builtin - contains a builtin string and function
